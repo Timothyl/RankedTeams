d399be49e0e454dbd8a177812d7d9b5bf8f6a830
Timothyl <timothyjlau@gmail.com>
2015-09-08 17:08:53 -0400

    split roster into two methods, one for id, one for names

--- League.rb
+++ League.rb
@@ -25,36 +25,44 @@ class League
     get("/v2.4/team/by-summoner/#{id}")
   end
 
-  def self.roster summoner, team_name
+  def self.get_roster_ids summoner, team_name
     id = League.summoner_name(summoner)[summoner]['id'].to_s
     summoners_teams = League.team(id)
     team_index = summoners_teams[id].index{|team| team['name'] == team_name}
     roster = summoners_teams[id][team_index]['roster']['memberList']
-    player_ids = ""
+    # player_ids = ""
     player_ids_array = []
     # binding.pry
     roster.each do |player|
-      player_ids << ',' << player["playerId"].to_s
+      # player_ids << ',' << player["playerId"].to_s
       player_ids_array << player["playerId"].to_s
     end
-    player_ids[0] = ""
-
-    player_names = []
-    players = summoner_id(player_ids)
-    player_ids_array.each do |player_id|
-      player_names << players[player_id]['name']
-    end
-
-
-    puts player_names
+    # player_ids[0] = ""
+    player_ids_array
 
   end
+
+  def self.get_roster_names summoner, team_name
+    id_array = League.get_roster_ids(summoner, team_name)
+    player_names = []
+    player_id_string = ""
+    id_array.each do |id|
+      player_id_string << ',' << id
+    end
+    player_id_string[0] = ""
+    players = summoner_id(player_id_string)
+    id_array.each do |player_id|
+      player_names << players[player_id]['name']
+    end
+    player_names
+  end
+
 
 end
 
 
-League.roster('quidestpro', 'Hello Kitties On Crack')
-
+League.get_roster_ids('quidestpro', 'Hello Kitties On Crack')
+puts League.get_roster_names('quidestpro', 'Hello Kitties On Crack')
 # binding.pry
 # File.open('temp.json', 'w') do |f|
 #   f.write(JSON.pretty_generate(summoners_teams[id][team_index]['roster']['memberList']))
